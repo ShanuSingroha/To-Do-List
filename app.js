e196272136f8d0ce0f10007a25393fd11fdcb26e
Shanu <shanusingroha@gmail.com>
2021-07-12 13:38:04 +0530

    addedfurtherrequirements

--- app.js
+++ app.js
@@ -123,6 +123,6 @@ app.post("/", function(req, res) {
          })
        }
       });
-      app.listen(3000, function() {
-        console.log("Server started on port 3000");
+      app.listen(process.env.PORT, function() {
+        console.log("Server started");
       });
